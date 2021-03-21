9bd4585ca369225312aa96c10bff5fda8bd03856
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2021-03-21 22:43:35 +0100

    feat: nat config changes queue

--- aws-ecs/cluster/nat/main.tf
+++ aws-ecs/cluster/nat/main.tf
@@ -1,3 +1,4 @@
+# Create a parameter for storing the NAT port forwarding config
 resource "aws_ssm_parameter" "config" {
   name  = "${var.name}-config"
   tags  = var.tags
@@ -9,6 +10,84 @@ resource "aws_ssm_parameter" "config" {
   }
 }
 
+# Create a queue so that the NAT instance can listen to config changes
+resource "aws_sqs_queue" "config_changes" {
+  name = "${var.name}-config-changes"
+  tags = var.tags
+
+  visibility_timeout_seconds = 60
+  message_retention_seconds  = 3600
+  receive_wait_time_seconds  = 20
+}
+
+# https://forums.aws.amazon.com/message.jspa?messageID=742808
+data "aws_iam_policy_document" "config_changes" {
+  statement {
+    actions   = ["sqs:SendMessage"]
+    resources = [aws_sqs_queue.config_changes.arn]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_sqs_queue_policy" "config_changes" {
+  queue_url = aws_sqs_queue.config_changes.id
+  policy    = data.aws_iam_policy_document.config_changes.json
+}
+
+# Push messages to the config changes queue whenever the parameter changes
+resource "aws_cloudwatch_event_rule" "config_changed" {
+  name     = "${var.name}-config-changed"
+  tags     = var.tags
+  role_arn = aws_iam_role.config_changed.arn
+
+  # https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/EventTypes.html#SSM-Parameter-Store-event-types
+  event_pattern = jsonencode({
+    source      = ["aws.ssm"]
+    detail-type = ["Parameter Store Change"]
+    resources   = [aws_ssm_parameter.config.arn]
+  })
+}
+
+resource "aws_cloudwatch_event_target" "queue_config_change" {
+  rule = aws_cloudwatch_event_rule.config_changed.id
+  arn  = aws_sqs_queue.config_changes.arn
+}
+
+data "aws_iam_policy_document" "config_changed_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "config_changed" {
+  statement {
+    actions   = ["sqs:SendMessage"]
+    resources = [aws_sqs_queue.config_changes.arn]
+  }
+}
+
+resource "aws_iam_role" "config_changed" {
+  name = "${var.name}-config-changed"
+  tags = var.tags
+
+  assume_role_policy = data.aws_iam_policy_document.config_changed_assume_role.json
+}
+
+resource "aws_iam_role_policy" "config_changed" {
+  role   = aws_iam_role.config_changed.name
+  name   = "self"
+  policy = data.aws_iam_policy_document.config_changed.json
+}
+
+# Create a static IP for the NAT instance
 resource "aws_eip" "this" {
   vpc  = true
   tags = var.tags
@@ -213,8 +292,8 @@ resource "aws_cloudwatch_event_rule" "launched" {
 }
 
 resource "aws_cloudwatch_event_target" "setup" {
-  arn  = module.setup_lambda.arn
   rule = aws_cloudwatch_event_rule.launched.id
+  arn  = module.setup_lambda.arn
 }
 
 resource "aws_lambda_permission" "setup" {
